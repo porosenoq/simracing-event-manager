854a8633c9e245df3ecaf1e6bd4e2a5c565204ca
Rosen Pashov <porosenoq@abv.bg>
2024-06-30 16:05:38 +0300

    Added example html markup in Events component
    Added some html markup and mockup data to Events component

--- simracing-event-manager/src/components/Events.jsx
+++ simracing-event-manager/src/components/Events.jsx
@@ -1,5 +1,32 @@
+import { Form, Button, Row, InputGroup } from 'react-bootstrap';
+import EventCard from './EventCard';
+
 export default function Events() {
     return(
-        <h1>Events page</h1>
+        <>
+            <h1>Events page:</h1>
+            <div className="container">
+                <Form>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Search events..."
+                            aria-label="Search events..."
+                            aria-describedby="basic-addon2"
+                        />
+                    <Button variant="warning" id="button-addon2">Search</Button>
+                    </InputGroup>
+                </Form>
+            </div>
+            <Row className="mx-0">
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+            </Row>
+        </>
     );
 }
