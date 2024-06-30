71642702fac42bb5b264039ceeaa145964f618ef
Rosen Pashov <porosenoq@abv.bg>
2024-06-30 13:56:52 +0300

    Added Footer component

--- simracing-event-manager/src/components/Footer.jsx
+++ simracing-event-manager/src/components/Footer.jsx
@@ -1,5 +1,18 @@
 export default function Footer() {
     return (
-        <h3>Footer &copy; 2024</h3>
+        <>
+        <div className="bg-dark text-light mx-0">
+  <footer className="py-3">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <li className="nav-item"><a href="#" className="nav-link px-2 ">Home</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 ">Features</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 ">Pricing</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 ">FAQs</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 ">About</a></li>
+    </ul>
+    <p className="text-center ">© 2024 Simracing Event Manager</p>
+  </footer>
+</div>
+        </>
     );
 }
