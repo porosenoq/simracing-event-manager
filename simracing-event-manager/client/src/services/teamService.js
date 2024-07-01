e5250e668bd86f1587aaf7ae50e253c24b7ae098
Rosen Pashov <porosenoq@abv.bg>
2024-07-01 17:41:10 +0300

    created teamsService for managing teams

--- simracing-event-manager/client/src/services/teamService.js
+++ simracing-event-manager/client/src/services/teamService.js
@@ -2,33 +2,33 @@ import { del, get, post, put } from "../utils/request";
 
 
 const endpoints = {
-    events: 'http://localhost:3030/data/events/',
+    teams: 'http://localhost:3030/data/teams/',
 }
 
 // TODO --> change user object
 
 export async function getAll(){
-    const result = await get(endpoints.events);
+    const result = await get(endpoints.teams);
     // ако не хвърли exception 
     return result;
 }
 
 export async function getById(id) {
-    const result = await get(endpoints.events + id);
+    const result = await get(endpoints.teams + id);
     return result;
 }
 
 export async function create(data) {
-    const result = await post(endpoints.events, data);
+    const result = await post(endpoints.teams, data);
     return result;
 }
 
 export async function update(id, data) {
-    const result = await put(endpoints.events + id, data);
+    const result = await put(endpoints.teams + id, data);
     return result;
 }
 
 export async function deleteById(id) {
-    const result = await del(endpoints.events + id);
+    const result = await del(endpoints.teams + id);
     return result;
 }
