f627459fe220fc56a94b96859b979220dfb1a4b0
Rosen Pashov <porosenoq@abv.bg>
2024-07-03 11:29:30 +0300

    Fixed duplicate form control id's

--- simracing-event-manager/client/src/components/CreateEvent.jsx
+++ simracing-event-manager/client/src/components/CreateEvent.jsx
@@ -24,7 +24,7 @@ export default function CreateEvent() {
     <Row className="mx-0">
       <Col md={4} className='container py-5'>
     <Form onSubmit={submitHandler}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="eventName">
         <Form.Label>Event name</Form.Label>
         <Form.Control type="text" name="name" placeholder="Enter event name" />
         <Form.Text className="text-muted">
@@ -32,7 +32,7 @@ export default function CreateEvent() {
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="eventDescription">
         <Form.Label>Event description</Form.Label>
         <Form.Control style={{ height: 150 }} as="textarea" name="description" placeholder="Short description about the event" />
         <Form.Text className="text-muted">
@@ -40,7 +40,7 @@ export default function CreateEvent() {
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="eventImage">
         <Form.Label>Event image</Form.Label>
         <Form.Control type="text" name="image" placeholder="URL to event image" />
       </Form.Group>
