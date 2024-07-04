56e8fc500e8cb62b086b0d07b6e7974c13920755
Rosen Pashov <porosenoq@abv.bg>
2024-07-04 13:44:18 +0300

    Added functionality to add teams

--- simracing-event-manager/client/src/components/CreateTeam.jsx
+++ simracing-event-manager/client/src/components/CreateTeam.jsx
@@ -1,23 +1,47 @@
 import { Button, Col, Form, Row } from 'react-bootstrap';
+import { create } from '../services/teamService';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../contexts/authContext';
 
 export default function CreateTeam() {
+
+  const navigate = useNavigate();
+  const {logoutHandler} = useContext(AuthContext);
+
+  const submitHandler = async (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const teamData = Object.fromEntries(formData);
+
+      try {
+        const result = await create(teamData);
+        console.log(result);
+      } catch(err) {
+        logoutHandler();
+        navigate('/login')
+      }
+      
+      navigate('/events');
+  }
+
     return (
         <>
     <h2>Create team:</h2>
     <Row className="mx-0">
       <Col md={4} className='container py-5'>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form onSubmit={submitHandler}>
+      <Form.Group className="mb-3" controlId="teamName">
         <Form.Label>Team name</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter team name" />
+        <Form.Control type="text" name="name" placeholder="Enter team name" />
         <Form.Text className="text-muted">
           {/*{errors ? errors : null}*/}
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="teamImage">
         <Form.Label>Team avatar</Form.Label>
-        <Form.Control type="password" name="password" placeholder="URL to team avatar image" />
+        <Form.Control type="text" name="image" placeholder="URL to team avatar image" />
       </Form.Group>
       <Button variant="warning" type="submit">
         Create team
--- simracing-event-manager/client/src/components/Teams.jsx
+++ simracing-event-manager/client/src/components/Teams.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { getAll } from '../services/teamService';
+import { ListGroup } from 'react-bootstrap';
+
 export default function Teams() {
+
+    const [teams, setTeams] = useState();
+
+    useEffect(() => {
+        async function loadTeams() {
+            const teams = await getAll();
+            setTeams(teams);
+        }
+        loadTeams();
+    }, []);
+
     return(
         <>
-            <h1>Teams page</h1>
+            <div className="container my-3">
+            <ListGroup>
+            {teams?.map(t => <ListGroup.Item className="bg-dark text-white" key={t._id}>{t.name}</ListGroup.Item>)}
+            </ListGroup>
+            </div>
         </>
     );
 }
