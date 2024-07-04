f0d75ecc5c5b1342c21cc3d9f281143bd680f9b5
Rosen Pashov <porosenoq@abv.bg>
2024-07-04 13:58:02 +0300

    Added functionality for when grid is full

--- simracing-event-manager/client/src/components/EventCard.jsx
+++ simracing-event-manager/client/src/components/EventCard.jsx
@@ -8,11 +8,14 @@ export default function EventCard({event}) {
 
   const {auth} = useContext(AuthContext);
   const [isSignedUp, setIsSignedUp] = useState(false);
+  const [gridFull, setGridFull] = useState(false);
 
   useEffect(() => {
     async function loadEventData() {
       const eventData = await getById(event._id);
       const isSubscribed = eventData.subscribers.includes(auth._id);
+      const full = eventData.subscribers.length == eventData.gridSize;
+      setGridFull(full);
       setIsSignedUp(isSubscribed);
     }
     loadEventData();
@@ -37,14 +40,15 @@ export default function EventCard({event}) {
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush event-list-items">
-        <ListGroup.Item className="bg-dark text-white event-list-items first-item">Registration: <Badge pill bg="success">{event.subscribers.length}/{event.gridSize}</Badge></ListGroup.Item>
+        <ListGroup.Item className="bg-dark text-white event-list-items first-item">Registration: <Badge pill bg={gridFull ? 'danger' : 'success'}>{event.subscribers.length}/{event.gridSize}</Badge></ListGroup.Item>
         <ListGroup.Item className="bg-dark text-white event-list-items">Event type: {event.type == 'both' ? 'Solo/Team' : event.type}</ListGroup.Item>
         <ListGroup.Item className="bg-dark text-white event-list-items">Category: {event.category.map(c => <span key={c} className='mx-1'>{c}</span>)}</ListGroup.Item>
         <ListGroup.Item className="bg-dark text-white event-list-items">Driver stint time: 60m</ListGroup.Item>
         <ListGroup.Item className="bg-dark text-white event-list-items">Event date: 05/07/2024 18:00UTC</ListGroup.Item>
       </ListGroup>
       <Card.Body className="event-list-buttons-container">
-        {auth.email && !isSignedUp ? <Button onClick={() => signUphandler(event._id)} className="mx-2" variant="success">Sign up</Button> : null}
+        {auth.email && !isSignedUp && !gridFull ? <Button onClick={() => signUphandler(event._id)} className="mx-2" variant="success">Sign up</Button> : null}
+        {gridFull ? <Button variant="warning mx-2">Grid is currently full</Button> : null}
         {auth.email && isSignedUp ? <Button className="mx-2" variant="danger">Sign out</Button> : null}
       
       
