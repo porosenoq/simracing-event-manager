39d0f0aa2c31006119279ad86923ac3d99f29a67
Rosen Pashov <porosenoq@abv.bg>
2024-07-12 10:13:46 +0300

    MyEvents now show both events you created and events you signed up for

--- simracing-event-manager/client/src/components/events/MyEvents.jsx
+++ simracing-event-manager/client/src/components/events/MyEvents.jsx
@@ -1,5 +1,41 @@
+import { useContext, useEffect, useState } from 'react';
+import { getMyEvents, getMySignedUpEvents } from '../../services/eventService';
+import AuthContext from '../../contexts/authContext';
+import { Col, Container, Row } from 'react-bootstrap';
+
 export default function MyEvents() {
+
+    const { auth } = useContext(AuthContext);
+
+    const [myEvents, setMyEvents] = useState([]);
+    const [mySignUps, setMySignUps] = useState([]);
+
+    useEffect(() => {
+        async function loadMyEvents() {
+            const myEventsData = await getMyEvents(auth._id);
+            setMyEvents(myEventsData);
+
+            const mySignUpsData = await getMySignedUpEvents(auth._id);
+            setMySignUps(mySignUpsData);
+        }
+        loadMyEvents();
+    }, []);
+
     return (
-        <h1>My events</h1>
+        <>
+            <Container className='bg-dark my-3 py-3 rounded'>
+                <Row>
+                    <Col md={6}>
+                        <h2>Events created by me:</h2>
+                        {myEvents.map(e => <p key={e._id}>{e.name}</p>)}    
+                    </Col>
+
+                    <Col md={6}>
+                        <h2>Events I signed up for:</h2>
+                        {mySignUps.map(s => <p key={s._id}>{s.name}</p>)}
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
