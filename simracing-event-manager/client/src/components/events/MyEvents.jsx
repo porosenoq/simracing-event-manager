446e69a7638a764a000b7653dc873cac64fca274
Rosen Pashov <porosenoq@abv.bg>
2024-07-12 14:54:18 +0300

    MyEvents html structure changes

--- simracing-event-manager/client/src/components/events/MyEvents.jsx
+++ simracing-event-manager/client/src/components/events/MyEvents.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from 'react';
-import { getMyEvents, getMySignedUpEvents } from '../../services/eventService';
+import { Link } from 'react-router-dom';
+import { Button, Col, Container, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
+import { CheckCircle, Gear, Trash } from 'react-bootstrap-icons';
+
 import AuthContext from '../../contexts/authContext';
-import { Col, Container, Row } from 'react-bootstrap';
+import { getMyEvents, getMySignedUpEvents } from '../../services/eventService';
 
 export default function MyEvents() {
 
@@ -27,12 +30,63 @@ export default function MyEvents() {
                 <Row>
                     <Col md={6}>
                         <h2>Events created by me:</h2>
-                        {myEvents.map(e => <p key={e._id}>{e.name}</p>)}    
+                        <Row className="py-3">
+                            <Col md={9}>Event:</Col>
+                            <Col md={3}>Manage:</Col>
+                        </Row>
+                        {myEvents.length > 0 ? 
+                            myEvents.map(e => (
+                                <Row className="hover_row py-2" key={e._id}>
+                                    <Col md={9}>
+                                        <Link className="navbar-link" to={`/events/details/${e._id}`}>
+                                            {e.name + ' '}
+                                        </Link>
+                                    </Col>
+                                    <Col md={3}>
+                                        <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Publish</Tooltip>}>
+                                            <CheckCircle className="mx-1" size="24" color="#198754"/>
+                                        </OverlayTrigger>
+                                        <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Configure</Tooltip>}>
+                                            <Link className="config" to={`/events/${e._id}/configure`}>
+                                                <Gear className="mx-1" size="24" />
+                                            </Link>
+                                        </ OverlayTrigger>
+                                        <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Delete</Tooltip>}>
+                                            <Trash className="mx-1" color="#dc3545" size="24" />
+                                        </OverlayTrigger>
+                                    </Col>
+                                </Row>
+                            )) :
+                            <>
+                                <p>You haven't created any events yet!
+                                <Link className="navbar-link" to={'/events/create'}> Create</Link> your first one now!</p>
+                            </>
+                        }    
                     </Col>
 
                     <Col md={6}>
                         <h2>Events I signed up for:</h2>
-                        {mySignUps.map(s => <p key={s._id}>{s.name}</p>)}
+                        <Row className="py-3">
+                            <Col md={8}>Event:</Col>
+                            <Col md={3}>Actions:</Col>
+                        </Row>
+                        {mySignUps.length > 0 ? 
+                            mySignUps.map(s => (
+                                <Row key={s._id}>
+                                    <Col md={8}>
+                                        <p key={s._id}>
+                                            <Link className="navbar-link" to={`/events/${s._id}`}>{s.name}</Link>
+                                        </p>
+                                    </Col>
+                                    <Col md={3}>
+                                        <Button size="sm" variant="danger">Sign out</Button>
+                                    </Col>
+                                </Row>
+                            )) :
+                            <>
+                                <p>You haven't signed up for an event yet! Head over to <Link className="navbar-link" to={'/events'}>events</Link> page and start racing now!</p>
+                            </>
+                        }
                     </Col>
                 </Row>
             </Container>
