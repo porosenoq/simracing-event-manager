92e1448d976fd19fc2545dfb00fccb150c4fcf0a
Rosen Pashov <porosenoq@abv.bg>
2024-07-15 13:23:40 +0300

    Added a link to team details and option to follow a team

--- simracing-event-manager/client/src/components/teams/Teams.jsx
+++ simracing-event-manager/client/src/components/teams/Teams.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAll } from '../../services/teamService';
-import { Card, Container, ListGroup } from 'react-bootstrap';
+import { Button, Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Teams() {
 
@@ -20,7 +21,7 @@ export default function Teams() {
                 <Card className="text-white bg-dark">
                     <Card.Body>
                     <ListGroup>
-                        {teams?.map(t => <ListGroup.Item className="bg-dark text-white" key={t._id}>{t.name}</ListGroup.Item>)}
+                        {teams?.map(t => <ListGroup.Item className="bg-dark text-white" key={t._id}><Row><Col md="8"><Link className="navbar-link" to={`/teams/${t._id}`}>{t.name}</Link></Col><Col md="3"><Button size="sm" variant="success">follow</Button></Col></Row></ListGroup.Item>)}
                     </ListGroup>
                 </Card.Body>
                 </Card>
