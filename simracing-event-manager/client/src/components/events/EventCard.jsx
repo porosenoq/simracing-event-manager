51489b1203e8ac77019864d93dc5fd6f90b674fb
Rosen Pashov <porosenoq@abv.bg>
2024-07-16 17:44:30 +0300

    Dynamically load event config data from backend
    It is no longer hardcoded in the template

--- simracing-event-manager/client/src/components/events/EventCard.jsx
+++ simracing-event-manager/client/src/components/events/EventCard.jsx
@@ -31,37 +31,42 @@ export default function EventCard({event}) {
   async function signUphandler() {
     const eventSubscribers = event.subscribers;
     eventSubscribers.push({ _id: auth._id });
-    const response = await update(event._id, {...event, subscribers: eventSubscribers});
-    console.log(response);
+    await update(event._id, {...event, subscribers: eventSubscribers});
     setIsSignedUp(true);
+  }
+
+  async function signOutHandler() {
+    const eventSubscribers = event.subscribers;
+    const subscription = eventSubscribers.findIndex(s => s._id == auth._id);
+    eventSubscribers.splice(subscription, 1);
+    await update(event._id, {...event, subscribers: eventSubscribers});
+    setIsSignedUp(false);
   }
 
     return (
         <Col md={3} className="my-3">
-        <Card className="bg-dark text-white event-card" style={{ width: '20rem', marginLeft: 'auto', marginRight: 'auto' }}>
-      <Card.Img variant="top" className="card-image-h" src={event.image} />
-      <Card.Body>
-        <Card.Title><Link to={`/events/details/${event._id}`} className='event-title'>{event.name}</Link></Card.Title>
-        <Card.Text className="event-description">
-          {event.description.length > 50 ? <>{event.description.substring(0, 50)}... <Link to={'/events/details/' + event._id} className='navbar-link'>read more</Link></> : event.description}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush event-list-items">
-        <ListGroup.Item className="bg-dark text-white event-list-items first-item">Registration: <Badge pill bg={gridFull ? 'danger' : 'success'}>{event.subscribers.length}/{event.gridSize}</Badge></ListGroup.Item>
-        <ListGroup.Item className="bg-dark text-white event-list-items">Event type: {event.type == 'both' ? 'Solo/Team' : event.type}</ListGroup.Item>
-        <ListGroup.Item className="bg-dark text-white event-list-items">Category: {event.category.map(c => <span key={c} className='mx-1'>{c}</span>)}</ListGroup.Item>
-        <ListGroup.Item className="bg-dark text-white event-list-items">Driver stint time: 60m</ListGroup.Item>
-        <ListGroup.Item className="bg-dark text-white event-list-items">Event date: 05/07/2024 18:00UTC</ListGroup.Item>
-      </ListGroup>
-      <Card.Body className="event-list-buttons-container">
-        {!auth.email ? <><span>Want to participate?<Link className='navbar-link nav-link'>Login</Link></span></> : null}
-        {auth.email && !isSignedUp && !gridFull ? <Button onClick={() => signUphandler(event._id)} className="mx-2" variant="success">Sign up</Button> : null}
-        {gridFull && !isSignedUp ? <Button disabled variant="warning mx-2">Grid is currently full</Button> : null}
-        {auth.email && isSignedUp ? <Button className="mx-2" variant="danger">Sign out</Button> : null}
-      
-      
-      </Card.Body>
-    </Card>
+          <Card className="bg-dark text-white event-card" style={{ width: '20rem', marginLeft: 'auto', marginRight: 'auto' }}>
+          <Card.Img variant="top" className="card-image-h" src={event.image} />
+          <Card.Body>
+            <Card.Title><Link to={`/events/details/${event._id}`} className='event-title'>{event.name}</Link></Card.Title>
+            <Card.Text className="event-description">
+              {event.description.length > 50 ? <>{event.description.substring(0, 50)}... <Link to={'/events/details/' + event._id} className='navbar-link'>read more</Link></> : event.description}
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush event-list-items">
+            <ListGroup.Item className="bg-dark text-white event-list-items first-item">Registration: <Badge pill bg={gridFull ? 'danger' : 'success'}>{event.subscribers.length}/{event.gridSize}</Badge></ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-white event-list-items">Event type: {event.type == 'both' ? 'Solo/Team' : event.type}</ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-white event-list-items">Category: {event.category.map(c => <span key={c} className='mx-1'>{c}</span>)}</ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-white event-list-items">Driver stint time: 60m</ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-white event-list-items">Event date: 05/07/2024 18:00UTC</ListGroup.Item>
+          </ListGroup>
+          <Card.Body className="event-list-buttons-container">
+           {!auth.email ? <><span>Want to participate?<Link className='navbar-link nav-link'>Login</Link></span></> : null}
+           {auth.email && !isSignedUp && !gridFull ? <Button onClick={() => signUphandler(event._id)} className="mx-2" variant="success">Sign up</Button> : null}
+           {gridFull && !isSignedUp ? <Button disabled variant="warning mx-2">Grid is currently full</Button> : null}
+           {auth.email && isSignedUp ? <Button onClick={() => signOutHandler(event._id)} className="mx-2" variant="danger">Sign out</Button> : null}
+          </Card.Body>
+          </Card>
         </Col>
     );
 }
