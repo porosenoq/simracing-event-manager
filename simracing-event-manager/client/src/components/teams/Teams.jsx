b66b449dbd592cfb89358b2c9eb793f8b7ab7c35
Rosen Pashov <porosenoq@abv.bg>
2024-07-16 17:53:42 +0300

    Show "follow" button on teams only if logged in

--- simracing-event-manager/client/src/components/teams/Teams.jsx
+++ simracing-event-manager/client/src/components/teams/Teams.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { getAll } from '../../services/teamService';
 import { Button, Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../contexts/authContext';
 
 export default function Teams() {
 
+    const { auth } = useContext(AuthContext);
     const [teams, setTeams] = useState();
 
     useEffect(() => {
@@ -21,7 +23,7 @@ export default function Teams() {
                 <Card className="text-white bg-dark">
                     <Card.Body>
                     <ListGroup>
-                        {teams?.map(t => <ListGroup.Item className="bg-dark text-white" key={t._id}><Row><Col md="8"><Link className="navbar-link" to={`/teams/${t._id}`}>{t.name}</Link></Col><Col md="3"><Button size="sm" variant="success">follow</Button></Col></Row></ListGroup.Item>)}
+                        {teams?.map(t => <ListGroup.Item className="bg-dark text-white" key={t._id}><Row><Col md={auth._id ? 8 : 12}><Link className="navbar-link" to={`/teams/${t._id}`}>{t.name}</Link></Col>{auth._id ? <Col md="3"><Button size="sm" variant="success">follow</Button></Col> : null }</Row></ListGroup.Item>)}
                     </ListGroup>
                 </Card.Body>
                 </Card>
