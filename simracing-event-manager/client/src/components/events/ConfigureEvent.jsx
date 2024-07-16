9bfa04b0d5437691369727523d28cde93d15f34f
Rosen Pashov <porosenoq@abv.bg>
2024-07-16 17:43:05 +0300

    Created component for configuring event parameters

---
+++ simracing-event-manager/client/src/components/events/ConfigureEvent.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getById } from '../../services/eventService';
+import { useParams } from 'react-router-dom';
+
+export default function ConfigureEvent() {
+
+    const { id } = useParams();
+    const [event, setEvent] = useState();
+    
+    useEffect(() => {
+        (async function loadEvent() {
+            const eventData = await getById(id);
+            setEvent(eventData);
+        })();
+    }, []);
+    
+    return(
+        <>
+        <h2>Configure</h2>
+
+        {event.name}
+        </>
+    );
+}
