0d438b53f5e65911a07b74715fd52095ba9754ab
Rosen Pashov <porosenoq@abv.bg>
2024-07-17 12:10:44 +0300

    Show grid full message only for logged in users

--- simracing-event-manager/client/src/components/events/EventCard.jsx
+++ simracing-event-manager/client/src/components/events/EventCard.jsx
@@ -63,7 +63,7 @@ export default function EventCard({event}) {
           <Card.Body className="event-list-buttons-container">
            {!auth.email ? <><span>Want to participate?<Link className='navbar-link nav-link'>Login</Link></span></> : null}
            {auth.email && !isSignedUp && !gridFull ? <Button onClick={() => signUphandler(event._id)} className="mx-2" variant="success">Sign up</Button> : null}
-           {gridFull && !isSignedUp ? <Button disabled variant="warning mx-2">Grid is currently full</Button> : null}
+           {gridFull && !isSignedUp && auth.email ? <Button disabled variant="warning mx-2">Grid is currently full</Button> : null}
            {auth.email && isSignedUp ? <Button onClick={() => signOutHandler(event._id)} className="mx-2" variant="danger">Sign out</Button> : null}
           </Card.Body>
           </Card>
